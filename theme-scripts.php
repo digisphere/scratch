df74fef3c8f6be9e235208dbaa54916322c47a64
Amit Moreno <amitm@codeoasis.com>
2015-10-02 07:42:50 +0300

    modifing bootstrap styles to sass

--- theme-scripts.php
+++ theme-scripts.php
@@ -4,10 +4,10 @@
 function scratch_scripts() {
 	
 	// Styles
-	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap/bootstrap.min.css' );
-	wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/css/font-awesome.min.css' );
-	wp_enqueue_style( 'main-style', get_template_directory_uri() . '/css/main.css' );
-	wp_enqueue_style( 'style', get_stylesheet_uri() );
+	wp_enqueue_style( 'font-awesome',	get_template_directory_uri() . '/css/font-awesome.min.css' );
+	wp_enqueue_style( 'bootstrap',		get_template_directory_uri() . '/css/bootstrap-styles.css' );
+	wp_enqueue_style( 'main-style',		get_template_directory_uri() . '/css/main.css' );
+	wp_enqueue_style( 'style',			get_stylesheet_uri() );
 
 	// Scripts - jQuery dependency
 	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap/bootstrap.min.js', array('jquery'),false,false );
